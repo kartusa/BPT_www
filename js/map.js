efc94e7823cffa92dc3e5dca6791df7f84f82663
kartusa <24987907+kartusa@users.noreply.github.com>
2019-04-13 11:32:23 +0200

    added

---
+++ js/map.js
@@ -0,0 +1,22 @@
+
+function selectTech() {
+    var image = document.getElementById("scenMap");
+    image.src = "img/01-home-bg4.jpg";
+    var text = document.getElementById("scenMapHeader");
+    text.innerHTML = '<h1 class="centruj">Technologiczne usługi projektowe</h1><hr><p><ul><li>Wdrożenia technologii w postaci projektu</li><li>Koncepcyjne studia wdrożeniowe technologii</li><li>Wstępne oraz pełne studia opłacalności oraz możliwości realizacji inwestycji</li><li>Bazowe projekty technologiczne</li><li>Konsultacje procesowe</li></li>Przeglądy bezpieczeństwa oraz wymogów eksploatacyjnych projektów instalacji - HAZOP</li><li>Weryfikacja i analizy instalacji technologicznych oraz pomocniczych będących w eksploatacji dla ustalenia sposobów poprawy jakości pracy</li></ul></p>';
+}
+
+function selectDesign() {
+    var image = document.getElementById("scenMap");
+    image.src = "img/01-home-bg3.jpg";
+    var text = document.getElementById("scenMapHeader");
+    text.innerHTML = '<h1 class="centruj">Realizacja projektów wielobranżowych</h1><hr><p><ul><li>Projekty bazowe</li><li>Projekty wykonawcze</li><li>Dokumentacja UDT</li><li>Dokumentacja powykonawcza</li></ul></p>';
+}
+
+function selectConsult() {
+    var image = document.getElementById("scenMap");
+    image.src = "img/01-home-bg1.jpg";
+    var text = document.getElementById("scenMapHeader");
+    text.innerHTML = '<h1 class="centruj">Usługi konsultacyjne</h1><hr><p><ul><li>Zarządzanie projektami wielobranżowymi</li><li>Zarządzanie umowami - <em>Claim Management</em></li><li>Zarządzanie oraz koordynacja realizacji umów z podwykonawcami</li><li>Inwentaryzacja</li></ul></p>';
+}
+
